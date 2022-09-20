7980e3cc39ed954478608cedc21a491202225a7a
Litzy15 <111078584+Litzy15@users.noreply.github.com>
2022-09-20 10:03:43 -0600

    Add files via upload

---
+++ Clase/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Acceso al panel
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet"  href="css/main.css" >
+  </head>
+  <body>
+    <div class="container">
+        <div class="row aling-items-center">
+        <form class ="" action="index.php" method="post" >  
+            <fieldset>
+            <legend>Datos de acceso</legend>
+            <label>Email:</label>
+            <input type= "text" name ="" value="" placeholder="Write here " >   
+            <label>Password:</label>
+            <input type= "password" name="" value ="" placeholder="********" >   
+            <button type= "submit" name="button" > Access </button> 
+            </fieldset>
+      </form>
+        </div>
+        
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
